bdc967a1e20e1e9e3ef80bc75e742321f0105593
chopokmado <chopokmado@gmail.com>
2020-06-21 14:04:22 +0900

    스테이지 4 사용자가 입력한 좌표정보로 다음 좌표에 X 돌을 놓는다. - 사용자 입력값을 검증한다. - 놓으려고 하는 좌표의 위치 정보를 검증한다.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,29 +1,41 @@
 package tictactoe;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Enter cells: ");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
 
-        GameInfo gameInfo = makeGameInfo(line);
+        GameInfo gameInfo = new GameInfo(line);
+        gameInfo.printInfo();
+
+
+        boolean b;
+        do {
+            System.out.print("Enter the coordinates: ");
+            String nextItem = scanner.nextLine();
+            b = gameInfo.pushNextItem(nextItem);
+        } while (b == false);
 
         gameInfo.printInfo();
-        gameInfo.printGameResult();
-    }
 
-    private static GameInfo makeGameInfo(String line) {
-        return new GameInfo(line);
+        //gameInfo.printGameResult();
     }
 
     public static class GameInfo {
+        
+        private Map<String, String> reversePointInfo;
         private String[][] basicInfo;
         public GameInfo(String line) {
 
-            String[] chars = line.split("");
+            String removedUnderLine = line.replace("_", " ");
+            String[] chars = removedUnderLine.split("");
 
             String[][] basicInfo = {
                     {chars[0], chars[1], chars[2]},
@@ -32,6 +44,21 @@ public class Main {
             };
 
             this.basicInfo = basicInfo;
+            this.reversePointInfo = makeReversePointInfo();
+        }
+
+        private Map<String, String> makeReversePointInfo() {
+            Map<String, String> reversePointInfo = new HashMap<>();
+            reversePointInfo.put("1 1", "2 0");
+            reversePointInfo.put("1 2", "1 0");
+            reversePointInfo.put("1 3", "0 0");
+            reversePointInfo.put("2 1", "2 1");
+            reversePointInfo.put("2 2", "1 1");
+            reversePointInfo.put("2 3", "0 1");
+            reversePointInfo.put("3 1", "2 2");
+            reversePointInfo.put("3 2", "1 2");
+            reversePointInfo.put("3 3", "0 2");
+            return reversePointInfo;
         }
 
         public void printInfo() {
@@ -132,6 +159,42 @@ public class Main {
             }
 
             return false;
+        }
+
+        public boolean pushNextItem(String nextItem) {
+            // validation user input
+            try {
+                String[] nextItemArray = nextItem.split(" ");
+                int xPos = Integer.valueOf(nextItemArray[0]);
+                int yPos = Integer.valueOf(nextItemArray[1]);
+
+                if (xPos > 3 || yPos > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    return false;
+                }
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
+                return false;
+            }
+
+            // 사용자가 입력한 좌표로 실제 배열 좌표 정보를 가져온다.
+            String reversedPointInfo = reversePointInfo.get(nextItem);
+            String[] reversedNextItemArray = reversedPointInfo.split(" ");
+            int xPos = Integer.valueOf(reversedNextItemArray[0]);
+            int yPos = Integer.valueOf(reversedNextItemArray[1]);
+
+            // 이미 값이 있으면 에러처리
+            // 없으면 특정 값을 입력한다.
+            String currentValue = basicInfo[xPos][yPos];
+            if (!currentValue.equals(" ")) {
+                System.out.println("This cell is occupied! Choose another one!");
+                return false;
+            } else {
+                basicInfo[xPos][yPos] = "X";
+                return true;
+            }
+
+
         }
     }
 }
